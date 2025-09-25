98ddb3a465ffdbaa6b52a421f657348ddded8b1d
Nathaniel Audrian <nathaniel.odi@gmail.com>
2025-09-25 17:11:40 +0700

    Add RestApiResponse

---
+++ src/main/java/org/jdt16/user4a/dto/response/RestApiResponse.java
@@ -0,0 +1,24 @@
+package org.jdt16.user4a.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RestApiResponse<T> {
+    @JsonProperty("code")
+    private Integer restApiResponseCode;
+
+    @JsonProperty("results")
+    private T restApiResponseResults;
+
+    @JsonProperty("message")
+    private String restApiResponseMessage;
+}
