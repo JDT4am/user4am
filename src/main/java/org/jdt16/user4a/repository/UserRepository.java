ba83bafafa148a36f82eaec52a1856dd4f82510b
Nathaniel Audrian <nathaniel.odi@gmail.com>
2025-09-25 17:32:45 +0700

    Fix import

--- src/main/java/org/jdt16/user4a/repository/UserRepository.java
+++ src/main/java/org/jdt16/user4a/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package org.jdt16.user4a.repository;
 
-import org.jdt16.user4a.entity.dto.UserDTO;
+import org.jdt16.user4a.dto.entity.UserDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
