5b5687909c0696f9a57b816259fac0041c283d4e
MOH. FAHRI WIJAYA <fahriw44@gmail.com>
2025-09-25 16:35:55 +0700

    first commit

---
+++ src/main/java/org/jdt16/user4a/User4aApplication.java
@@ -0,0 +1,13 @@
+package org.jdt16.user4a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class User4aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(User4aApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jdt16/user4a/User4aApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jdt16.user4a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class User4aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
